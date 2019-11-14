010f3adf85b8765f3785cfa175c9cc40d9526d32
Asim-A <asimabazi@hotmail.no>
2019-11-14 20:36:55 +0100

    project cleanup

--- Controllers/FaqController.cs
+++ Controllers/FaqController.cs
@@ -34,56 +34,6 @@ namespace FAQApi.Controllers
             
         }
 
-        public void generateTestSet()
-        {
-            ICollection<Question> q = new List<Question>();
-            ICollection<Question> q2 = new List<Question>();
-
-            ICollection<Subcategory> subcats = new List<Subcategory>();
-
-            foreach (var item in questions)
-            {
-
-                var d = new Question
-                {
-                    question_body = item
-                };
-
-                q.Add(d);
-
-            }
-
-            q2.Add(new Question
-            {
-                question_body = "HVA SKJER MED VY?!"
-            });
-
-            Subcategory s1 = new Subcategory
-            {
-                subcategory_title = "Buss",
-                Questions = q
-            };
-
-            Subcategory s2 = new Subcategory
-            {
-                subcategory_title = "VY!",
-                Questions = q2
-            };
-
-            subcats.Add(s1);
-            subcats.Add(s2);
-
-
-
-            Category category = new Category
-            {
-                category_body = "Diverse",
-                Subcategories = subcats
-            };
-
-            context.categories.Add(category);
-            context.SaveChanges();
-        }
 
 
 
